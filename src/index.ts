034ab62dfbdaca32dcca4f473074ffcc38cd06b3
Avinash Verma <avinashverma.it@gmail.com>
2020-06-19 09:59:45 +0530

    Expose Application and move all event dispatchers / listeners to index html

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import { Application } from './App';
-import { BACKGROUND_IMAGE } from './globalConstants';
-import { IApplicationData } from './interfaces';
 
 import './style.css';
 
-document.addEventListener('collageExport', (event) => {
-  console.log((event as CustomEvent).detail);
-});
-
-document.addEventListener('collageImport', (event) => {
-  new Application().renderApplication(
-    (event as CustomEvent).detail as IApplicationData,
-  );
-});
+(window as any).Application = Application;
 
 document.dispatchEvent(new CustomEvent('collageReady'));
